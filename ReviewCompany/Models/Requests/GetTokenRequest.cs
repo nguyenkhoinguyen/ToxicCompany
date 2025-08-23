310c378177590553f8b079c537a293dcea44918e
nguyennk <nguyennkgtvt@gmail.com>
2025-08-23 23:45:47 +0700

    add more infor

--- ReviewCompany/Models/Requests/GetTokenRequest.cs
+++ ReviewCompany/Models/Requests/GetTokenRequest.cs
@@ -1,7 +1,28 @@
-﻿namespace ReviewCompany.Models.Requests
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ReviewCompany.Models.Requests
 {
     public class GetTokenRequest
     {
-        public string ApiKey { get; set; } = string.Empty; // REQUIRED
+        [Required(ErrorMessage = "ApiKey is required.")]
+        public string ApiKey { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "DeviceId is required.")]
+        public string DeviceId { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "DeviceName is required.")]
+        public string DeviceName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "IpAddress is required.")]
+        public string IpAddress { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "AppId is required.")]
+        public string AppId { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "AppCode is required.")]
+        public string AppCode { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Country is required.")]
+        public string Country { get; set; } = string.Empty;
     }
 }
